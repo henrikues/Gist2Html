3df0f8a20dcabd380ad1a6dfeb973ae843b78ec5
Henrikues <donatihenrique@hotmail.com>
2025-05-08 23:03:52 -0500

    Add replace based on file name

--- function_app.py
+++ function_app.py
@@ -5,26 +5,39 @@ import requests
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="gist2html")
-def gist2html(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def gist2Html(req: func.HttpRequest) -> func.HttpResponse:
+    functionUrl = req.url[:req.url.rfind("gist2html")]
+    gistId = req.params.get('gistId')
+    url = f'https://api.github.com/gists/{gistId}'
 
-    name = req.params.get('name')
-    url = f'https://api.github.com/gists/{name}'
-    if name:
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
-        session = requests.Session()
+    if gistId:
         response = requests.get(url)
         responseData = response.json()
-        raw_url = responseData['files']['index.html']['raw_url']
-        indexFile = requests.get(raw_url).text
-        return func.HttpResponse(
-            indexFile,
-            mimetype="text/html",
-            charset="utf-8",
-            status_code=200
-        )
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        files = responseData['files']
+        rawUrl = files['index.html']['raw_url']
+        indexFile = requests.get(rawUrl).text
+        for key, value in files.items():
+            if key != 'index.html':
+                loopRawUrl = value['raw_url']
+                encodedLoopRawUrl = requests.utils.quote(loopRawUrl, safe='')
+                indexFile = indexFile.replace(key, functionUrl + "rawfile2html?url=" + encodedLoopRawUrl)
+
+    return func.HttpResponse(
+        indexFile,
+        mimetype="text/html",
+        charset="utf-8",
+        status_code=200
+    )
+
+#Since the gist raw_url returns with the mime type of "text/plain", browsers won't read it as JS nor CSS
+#Now "text/html" seems to be good enough for Chrome :D
+@app.route(route="rawfile2html")
+def rawFile2Html(req: func.HttpRequest) -> func.HttpResponse:
+    raw_url = req.params.get('url')
+    indexFile = requests.get(raw_url).text
+    return func.HttpResponse(
+        indexFile,
+        mimetype="text/html",
+        charset="utf-8",
+        status_code=200
+    )
